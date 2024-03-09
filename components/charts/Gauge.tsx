da9f6a1ffd6941b150882fcff6e28a61ac2d0fba
syket-das <saiketsd23@gmail.com>
2024-03-09 15:41:18 +0530

    sd

--- components/charts/Gauge.tsx
+++ components/charts/Gauge.tsx
@@ -6,11 +6,62 @@ const Gauge = () => {
   const GaugeComponent = dynamic(() => import('react-gauge-component'), {
     ssr: false,
   });
+
+  const kbitsToMbits = (value: any) => {
+    if (value >= 1000) {
+      value = value / 1000;
+      if (Number.isInteger(value)) {
+        return value.toFixed(0) + ' mbit/s';
+      } else {
+        return value.toFixed(1) + ' mbit/s';
+      }
+    } else {
+      return value.toFixed(0) + ' kbit/s';
+    }
+  };
+
   return (
     <div className="w-full h-full ">
       <div className="flex flex-wrap justify-center items-center gap-4 min-h-full">
         <div className="w-[300px] h-[300px] ">
-          <GaugeComponent />
+          <GaugeComponent
+            arc={{
+              nbSubArcs: 150,
+              colorArray: ['#5BE12C', '#F5CD19', '#EA4228'],
+              width: 0.3,
+              padding: 0.003,
+            }}
+            labels={{
+              valueLabel: {
+                fontSize: 40,
+                formatTextValue: kbitsToMbits,
+              },
+              tickLabels: {
+                type: 'outer',
+                ticks: [
+                  { value: 100 },
+                  { value: 200 },
+                  { value: 300 },
+                  { value: 400 },
+                  { value: 500 },
+                  { value: 600 },
+                  { value: 700 },
+                  { value: 800 },
+                  { value: 900 },
+                  { value: 1000 },
+                  { value: 1500 },
+                  { value: 2000 },
+                  { value: 2500 },
+                  { value: 3000 },
+                ],
+                valueConfig: {
+                  formatTextValue: kbitsToMbits,
+                },
+              },
+            }}
+            value={900}
+            maxValue={3000}
+          />
 
           <p className="text-center text-md font-bold">AI/ML</p>
         </div>
